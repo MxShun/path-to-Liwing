f73f39cace774f079a7e4843912393226e9d849e
MxShun <16466322+MxShun@users.noreply.github.com>
2021-03-26 20:42:40 +0900

    Add barnacle-boy Usage

--- barnacle-boy.js
+++ barnacle-boy.js
@@ -5,20 +5,24 @@ exports.path = async (req, res) => {
   if (payload.type === 'url_verification') {
     return res.status(200).json({'challenge': payload.challenge});
   }
-  const url = payload.text;
-  if (!url || url === "") {
-    return res.status(200).end();
+
+  let message = payload.text;
+  let postText;
+  if (!message || message === "") {
+    postText = 'Usage: /path2liwing <path>';
+  }
+  else {
+    postText = (/^(.*)smb\:\/\/(.*)$/.test(message)) ?
+      `<lw://file/${message.replace('smb:','')}>` : `<lw://file/${message.replace(/\\/g, '/')}>`;
   }
 
-  const replacingText = (/^(.*)smb\:\/\/(.*)$/.test(url)) ?
-    `lw://file/${url.replace('smb:','')}` : `lw://file/${url.replace(/\\/g, '/')}`;
   try {
     await rp({
       'method' : 'POST',
       'uri' : payload.response_url,
       'headers' : {'Accept': 'application/json', 'Content-Type': 'application/json'},
       'json' : true,
-      'body' : {'text' : `<${replacingText}>`}
+      'body' : {'text' : postText}
     });
     console.log(`text:${payload.text}, user:${payload.user_name}`);
   } catch(e) {
